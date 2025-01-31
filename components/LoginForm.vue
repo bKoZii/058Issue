2375457d09a4f33853f1252a2fae2874910d65b8
bKoZii <bigkonkamon@gmail.com>
2025-01-31 10:00:26 +0700

    เพิ่มไฟล์ Code .vue

---
+++ components/LoginForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <input type="text" name="username" id="username" placeholder="Username">
+    <input type="password" name="password" id="password" placeholder="Password">
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const username = ref('');
+const password = ref('');
+</script>
---
+++ contact.vue
@@ -0,0 +1,39 @@
+<template>
+  <body bgcolor="#CC9900">
+    <table width="1238" height="530" border="0">
+      <tr>
+        <td width="23" rowspan="2">&nbsp;</td>
+        <td height="119" colspan="2" bgcolor="#006600">
+          <h1>
+            <center>
+              <font color="#FFFFFF">พรชัยอพาร์ตเมนต์</font>
+            </center>
+          </h1>
+          <Date />
+        </td>
+        <td width="43" rowspan="2">&nbsp;</td>
+      </tr>
+      <tr>
+        <td width="1007" bgcolor="#FFFFFF">
+          <img src="images/map.gif">
+        </td>
+        <td width="190" bgcolor="#FFFF66">
+          <ul>
+            <li><a href="/home">หน้าแรก</a></li>
+            <li><a href="/room">ห้องพัก&ราคา</a></li>
+            <li><a herf="/guest">สมุดเยี่ยม</a></li>
+            <li><a href="/contact">ติดต่อ</a></li>
+          </ul>
+          <div>
+            <LoginForm />
+          </div>
+        </td>
+      </tr>
+    </table>
+  </body>
+</template>
+
+<script setup>
+import LoginForm from '/components/LoginForm.vue';
+import Date from '/components/Date.vue';
+</script>
